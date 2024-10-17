f2794592607df72e897a7d628d1a4ed374e816db
hubert22143 <33151182+hubert22143@users.noreply.github.com>
2024-10-17 20:48:50 +0200

    Exercises

---
+++ exercises/School Assigments/Assigment 1/doc.exercises.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+/*
+void kelvinToCelsius(int kelvin){
+    double converser = -273.15 + kelvin;
+    printf("Przemiana %d kelvinow na celsjusze wynosi %.2f\n",kelvin,converser);
+}
+
+void celsiusToKelvin(int celsius){
+    double converser = 273.15 + celsius;
+    printf("Przemiana %d celsjuszow na kelviny wynosi %.2f\n",celsius,converser);
+}
+
+int rectengularVolume(){
+    int a,b,h;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&h);
+    int volume = a * b * h;
+    return volume;
+}
+
+double linearEquation(double a, double b){
+    double x;
+    if(a!=0){
+        x = -b/a;
+        return x;
+    }else{
+        if(b==0){
+            printf("Rownanie Tozsamosciowe,Rozwiazaniem jest kazda liczba x nalezaca do zbioru liczb rzeczywistych, nieskonczenie wiele rozwiazan\n");
+            return 1;
+        }else{
+            printf("Rownanie sprzeczne, brak rozwiazan\n");
+            return 0;
+        }
+    }
+
+}
+*/
+int main() {
+    /*
+    int x = 730;
+    long y = 106;
+    double z = 123.45;
+    double sum = 1 - z;
+    printf("%d w systemie szesnastkowym to %x \n",x,x);
+    printf("%ld w systemie osemkowym to %lo\n",y,y);
+    printf("Operacja 1 - y wyniesie %.2f\n",sum);
+    
+   kelvinToCelsius(250);
+   celsiusToKelvin(10);
+
+   int volume = rectengularVolume();
+    printf("%d",volume);
+
+      linearEquation(0,0);
+       */
+}

3017c982bd4305edbaabde1977e4610bc806f44a
hubert22143 <33151182+hubert22143@users.noreply.github.com>
2024-10-27 16:19:05 +0100

    Added backpack struct

--- exercises/cMiniGame/minigame.c
+++ exercises/cMiniGame/minigame.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 struct Player{
-    char* PlayerName;
-    char* PlayerVocation;
 
-    int PlayerLevel;
+    char* Name;
+    char* Vocation;
+    int Level;
 };
+struct ItemType{
+    char* assignCategory;
+    char* assignRarity;
+};
+struct Item{
+    char* name;
+    char* itemAttributes[5];
+    struct ItemType* type;
+};
+struct Backpack{
+    char* Name;
+    struct Item Items[10];
+    };
 struct PlayerEquipment{
-    char* playerWeapon;
-    char* playerHelmet;
-    char* playerArmor;
-    char* playerLegs;
-    char* playerBoots;
+    char* Weapon;
+    char* Helmet;
+    char* Armor;
+    char* Legs;
+    char* Boots;
+    char* Ring;
 
-    char* playerRing;
-    char* playerBackpack[10];
+    struct Backpack backpack;
 };
+
 struct Vocations{
-    char* vocationList[4];
+    char* available[4];
 };
 struct Weapons{
     char* Rod;
@@ -36,18 +51,33 @@ struct BeginningLocation{
 };
 void printPlayerInfo(struct Player* player)
 {
-    printf("Name: %s\n",player->PlayerName);
-    printf("Vocation: %s\n",player->PlayerVocation);
-    printf("Current level: %d\n",player->PlayerLevel);
+    printf("Name: %s\n",player->Name);
+    printf("Vocation: %s\n",player->Vocation);
+    printf("Current level: %d\n",player->Level);
 };
+void initializeBackpack(struct Backpack* backpack,const char* name){
+    backpack->Name = strdup(name);
+}
+int traverseInputToDigit(char* string) {
+    int num = 0;
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (string[i] >= '0' && string[i] <= '9') {
+            num = num * 10 + (string[i] - '0');
+        } else {
+            break;
+        }
+    }
+    return num;
+}
+
 void printPlayerEquipment(struct PlayerEquipment* equipment){
     char* equipmentContainer[] = {
-        equipment->playerWeapon,
-        equipment->playerHelmet,
-        equipment->playerArmor,
-        equipment->playerLegs,
-        equipment->playerBoots,
-        equipment->playerRing
+        equipment->Weapon,
+        equipment->Helmet,
+        equipment->Armor,
+        equipment->Legs,
+        equipment->Boots,
+        equipment->Ring
     };
         const char* equipmentLabels[] = {
         "Weapon",
@@ -65,42 +95,86 @@ void printPlayerEquipment(struct PlayerEquipment* equipment){
 
 }
 void printPlayerBackpack(struct PlayerEquipment* equipment) {
-    //will be implemented as the minigame expand
+for(int i = 0; i < 10; i++){
+    if(equipment->backpack.Items[i].name == NULL){
+        printf("%d : your slot is empty\n",i + 1);
+    }else{
+        printf("%d : %s",i+1,equipment->backpack.Items[i].name);
+    }
+}   
+};
+void interractWithBackpack(struct PlayerEquipment* equipment)
+{
+        printf("Type corresponding number index in order to interact with item or type !e to exit \n");
+    char temp[10];
+    scanf("%s",temp);
+    while (strcmp(temp,"!e") != 0)
+    {
+        int index = traverseInputToDigit(temp);
+        if(index >= 1 && index <=10){
+          if(equipment->backpack.Items[index - 1].name != NULL){
+            printf("Interracting with item: %d\n",index);
+          }else{
+            printf("Slot %d is empty\n",index);
+          }
+        }else{
+            printf("Invalid index, enter a valid number between 1 and 10\n");
+        }
+        
+    }
 };
 
+
 void freeAnyTakenMemory(struct PlayerEquipment* equipment,struct Player* player){
-free(player->PlayerName);
-free(player->PlayerVocation);
+free(player->Name);
+free(player->Vocation);
 
-
-free(equipment->playerWeapon);
-
-printf("Memory: MASLO !!!");
-
-
-free(equipment->playerHelmet);
-free(equipment->playerArmor);
-free(equipment->playerLegs);
-free(equipment->playerBoots);
-free(equipment->playerRing);
-
-
+free(equipment->Weapon);
+free(equipment->Helmet);
+free(equipment->Armor);
+free(equipment->Legs);
+free(equipment->Boots);
+free(equipment->Ring);
 }
+void preventDanglingPointers(struct PlayerEquipment* equipment,struct Player* player){
+    equipment->Armor = NULL;
+    equipment->Boots = NULL;
+    equipment->Helmet = NULL;
+    equipment->Ring = NULL;
+    equipment->Legs = NULL;
+    equipment->Weapon = NULL;
+    equipment->backpack.Name = NULL;
 
+    for (int i = 0; i < 10; i++) {
+        equipment->backpack.Items[i].name = NULL;
+        equipment->backpack.Items[i].type = NULL;
+        for (int j = 0; j < 5; j++) {
+            equipment->backpack.Items[i].itemAttributes[j] = NULL;
+        }
+    }
+    player->Name = NULL;
+    player->Vocation = NULL;
+}
 int InitializeAvailableMemory(struct PlayerEquipment* equipment,struct Player *player){
-    player->PlayerVocation = malloc(20 * sizeof(char));
-    player->PlayerName = malloc(16 * sizeof(char));
+    player->Vocation = malloc(20 * sizeof(char));
+    player->Name = malloc(16 * sizeof(char));
 
-    equipment->playerWeapon = malloc(30 * sizeof(char));
-    equipment->playerHelmet = malloc(30 * sizeof(char));
-    equipment->playerArmor= malloc(30 * sizeof(char));
-    equipment->playerLegs= malloc(30 * sizeof(char));
-    equipment->playerBoots = malloc(30 * sizeof(char));
-    equipment->playerRing = malloc(30 * sizeof(char));
-    if (!equipment->playerWeapon || !equipment->playerHelmet || 
-        !equipment->playerArmor || !equipment->playerLegs || 
-        !equipment->playerBoots || !equipment->playerRing ||
-        !player->PlayerVocation || !player->PlayerName) {
+    equipment->Weapon = malloc(30 * sizeof(char));
+    equipment->Helmet = malloc(30 * sizeof(char));
+    equipment->Armor= malloc(30 * sizeof(char));
+    equipment->Legs= malloc(30 * sizeof(char));
+    equipment->Boots = malloc(30 * sizeof(char));
+    equipment->Ring = malloc(30 * sizeof(char));
+
+    for (int i = 0; i < 10; i++) {
+    equipment->backpack.Items[i].name = malloc(30 * sizeof(char));
+    equipment->backpack.Items[i].type = malloc(sizeof(struct ItemType));
+    }
+
+    if (!equipment->Weapon || !equipment->Helmet || 
+        !equipment->Armor || !equipment->Legs || 
+        !equipment->Boots || !equipment->Ring ||
+        !player->Vocation || !player->Name) {
         printf("Memory allocation failed \n");
         return 0;
     };
@@ -113,10 +187,10 @@ void initializeBeginningEquipment(char* chosenCharacter,struct PlayerEquipment* 
     {
         if (strcmp(chosenCharacter,characterList[i])==0)
         {
-            sprintf(equipment->playerHelmet,"%s helmet",chosenCharacter);
-            sprintf(equipment->playerArmor,"%s Armor",chosenCharacter);
-            sprintf(equipment->playerLegs,"%s Legs",chosenCharacter);
-            sprintf(equipment->playerBoots,"%s Boots",chosenCharacter);
+            sprintf(equipment->Helmet,"%s helmet",chosenCharacter);
+            sprintf(equipment->Armor,"%s Armor",chosenCharacter);
+            sprintf(equipment->Legs,"%s Legs",chosenCharacter);
+            sprintf(equipment->Boots,"%s Boots",chosenCharacter);
         }
         
     }
@@ -125,11 +199,11 @@ void initializeBeginningEquipment(char* chosenCharacter,struct PlayerEquipment* 
 
 int initializePlayer(struct PlayerEquipment* equipment,struct Player* player) {
     InitializeAvailableMemory(equipment,player);
-    player->PlayerLevel = 1;
+    player->Level = 1;
     printf("Welcome in the game! As for beginning, please type your nickname (15 character maximum!):\n");
-    scanf("%s", player->PlayerName);
+    scanf("%s", player->Name);
 
-    if (strlen(player->PlayerName) > 15) {
+    if (strlen(player->Name) > 15) {
         printf("You're not allowed to have that long character!\n");
         //freeAnyTakenMemory(equipment,player);
         return 0;
@@ -140,10 +214,10 @@ int initializePlayer(struct PlayerEquipment* equipment,struct Player* player) {
 
 int selectVocation(struct Player* player, struct Weapons* weapon, struct Vocations* vocation, struct PlayerEquipment* equipment) {
     
-    vocation->vocationList[0] = "Paladin";
-    vocation->vocationList[1] = "Sorcerer";
-    vocation->vocationList[2] = "Druid";
-    vocation->vocationList[3] = "Knight";
+    vocation->available[0] = "Paladin";
+    vocation->available[1] = "Sorcerer";
+    vocation->available[2] = "Druid";
+    vocation->available[3] = "Knight";
 
     weapon->Rod = "Rod";
     weapon->Wand = "Wand";
@@ -154,12 +228,12 @@ int selectVocation(struct Player* player, struct Weapons* weapon, struct Vocatio
 
     char* uniqueWeapon = NULL;
 
-    printf("Welcome %s! You have four vocations to select: Paladin, Sorcerer, Druid, Knight. Please type your choice:\n", player->PlayerName);
-    scanf("%19s", player->PlayerVocation);
+    printf("Welcome %s! You have four vocations to select: Paladin, Sorcerer, Druid, Knight. Please type your choice:\n", player->Name);
+    scanf("%19s", player->Vocation);
 
     for (int i = 0; i < 4; i++) {
-        if (strcmp(player->PlayerVocation, vocation->vocationList[i]) == 0) {
-            initializeBeginningEquipment(player->PlayerVocation,equipment);
+        if (strcmp(player->Vocation, vocation->available[i]) == 0) {
+            initializeBeginningEquipment(player->Vocation,equipment);
             switch (i) {
                 case 0: uniqueWeapon = weapon->Bow; break;
                 case 1: uniqueWeapon = weapon->Wand; break;
@@ -178,11 +252,10 @@ int selectVocation(struct Player* player, struct Weapons* weapon, struct Vocatio
                     }
                     break;
             }
-
             if (uniqueWeapon != NULL) {
-                printf("You've selected %s.\n", player->PlayerVocation);
+                printf("You've selected %s.\n", player->Vocation);
                 printf("Your unique weapon is: %s.\n", uniqueWeapon);
-                strcpy(equipment->playerWeapon,uniqueWeapon);
+                strcpy(equipment->Weapon,uniqueWeapon);
                 return 1;
             }
         }
@@ -200,6 +273,8 @@ void playerMenu(struct Player* player,struct PlayerEquipment* equipment){
         printPlayerInfo(player);
     }else if (strcmp(temp,"!e") == 0){
         printPlayerEquipment(equipment);
+    }else if(strcmp(temp,"!b") == 0){
+        printPlayerBackpack(equipment);
     }
     if (!(strcmp(temp, "!c") == 0 || strcmp(temp,"!s") == 0 || strcmp(temp,"!e") ==0 || strcmp(temp,"!b") ==0)) {
             printf("You've misstyped the command you baka... don't worry there's another chance to enter it properly :) \n\n");
@@ -212,11 +287,13 @@ int main(){
     struct Player player;
     struct Weapons weapon;
     struct Vocations vocation;
+    initializeBackpack(&equipment.backpack,"Adventurer's Bag");
     if(initializePlayer(&equipment,&player)){
      if(selectVocation(&player,&weapon,&vocation,&equipment)){
        playerMenu(&player,&equipment);
      }
     }
     freeAnyTakenMemory(&equipment,&player);
+    preventDanglingPointers(&equipment,&player);
     return 0;
 }
